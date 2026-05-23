11d42ae17c91d4d6df4b31e3b198c3d4099bd08b
Michelle <262064@virtualwindow.co.za>
2026-05-23 10:51:59 +0200

    Js_editing

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const items = document.querySelectorAll("#carouselExample .carousel-item");
-  let index = 0;
+//search//
+function navigateToProduct() {
+  const searchInput = document.getElementById('creatureSearch');
+  if (!searchInput) return;
 
-  const showSlide = (newIndex) => {
+  const query = searchInput.value.trim();
+  
+  if (query === "") {
+    alert("Please enter a name to search.");
+    return;
+  }
+
+  window.location.href = `pages/adopt.html?search=${encodeURIComponent(query)}`;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.getElementById('creatureSearch');
+  if (searchInput) {
+    searchInput.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        navigateToProduct();
+      }
+    });
+  }
+});
+
+//carousel function//
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll(".carousel-item");
+  if(items.length > 0) {
+    let index = 0;
+    const showSlide = (newIndex) => {
     items[index].classList.remove("active");
     index = (newIndex + items.length) % items.length;
     items[index].classList.add("active");
   };
 
-  document.getElementById("prevBtn").addEventListener("click", () => showSlide(index - 1));
-  document.getElementById("nextBtn").addEventListener("click", () => showSlide(index + 1));
-})
+  const prevBtn = document.querySelector(".carousel-control-prev");
+  const nextBtn = document.querySelector(".carousel-control-next");
 
-//search//
+  if (prevBtn) prevBtn.addEventListener("click", () => showSlide(index - 1));
+  if (nextBtn) nextBtn.addEventListener("click", () => showSlide(index + 1));
+ }
+});
+
+
+
